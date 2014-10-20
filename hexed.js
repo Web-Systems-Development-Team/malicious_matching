71efbf9fa5c2fcf322bdc167c87a86b2c9afdadf
Grace W. <anantaya22@gmail.com>
2014-10-20 17:03:14 -0400

    hexed.js
    calculate_score function works now

--- hexed.js
+++ hexed.js
@@ -1,28 +1,50 @@
+var theColor=[];
+var ansColor=[];
+var start = 0;
+var end = 0;
+var difficulty = 0;
+
+
 $.fn.hexed = function() {
     make_sliders();
     $("#gen").click(function() { // need time
-        var start = new Date().getTime();
+        start = new Date().getMilliseconds();
+        //alert(start);
         var c=document.getElementById("goalCanvas");
         var ctx=c.getContext("2d");
         ctx.beginPath();
         ctx.arc(100,75,50,0,2*Math.PI);
         ctx.stroke();
-	ctx.fillStyle=randomColor();
+	    ctx.fillStyle=randomColor();
         ctx.fill();
+        //var d=document.getElementById("playerCanvas");
+        //d.hide();
     });
     $("#answer").click(function() {
-        var end = new Date().getTime();
+        //alert("click got it");
+        end = new Date().getMilliseconds();
+        //alert(end);
         var c=document.getElementById("playerCanvas");
         var ctx=c.getContext("2d");
         ctx.beginPath();
         ctx.arc(100,75,50,0,2*Math.PI);
         ctx.stroke();
-        ctx.fill(randomColor());        
+        var getR = parseInt($("#red_slider_number").text().trim(), 10);
+        //alert("r = " + getR);
+        var getG = parseInt($("#green_slider_number").text().trim(), 10);
+        var getB = parseInt($("#blue_slider_number").text().trim(), 10);
+        ansColor = [getR, getG, getB];
+        ctx.fillStyle = "#" + getR.toString(16) + getB.toString(16) + getG.toString(16);;
+        ctx.fill();
+        alert("percent off = " + percent_off() + "%");
+        alert("score = " + calculate_score());
+
+        //alert(ansColor[0] + " " + ansColor[1] + " " + ansColor[2]);     
     });
     return this;
 }
 
-theColor=[];                       
+                    
 function randomColor(){ //determine random rgb
     var r=Math.floor((Math.random() * 255)); //range of 0-255
     var g=Math.floor((Math.random() * 255));
@@ -46,9 +68,9 @@ function make_sliders() {
 //determine the percent difference between the actual and
 //expected r, g, b variables
 function percent_off() {
-    return ((theColor[0] - $("#red_slider").slider("value")) +
-            (theColor[1] - $("#green_slider").slider("value")) +
-            (theColor[2] - $("#blue_slider").slider("value"))) * 100 / 765;
+    return ((theColor[0] - ansColor[0]) +
+            (theColor[1] - ansColor[1]) +
+            (theColor[2] - ansColor[2])) * 100 / 765;
     /* division by 765 because it would otherwise be divided by 255 and 
        then by 3 to take the average */
 }
@@ -56,9 +78,14 @@ function percent_off() {
 // determine score
 // ((15 – difficulty – percent_off) / (15 – difficulty)) * (15000 – milliseconds_taken)
 //not finished yet, the /*time*/ must be replaced
-/*
+
 function calculate_score() {
     var weighted_diff = 15 - difficulty;
-    var score = (weighted_diff - percent_off()) / (weighted_diff * /*time*);
+    //alert("start:" + start);
+    //alert("end:" + end);
+    var score = (weighted_diff - percent_off()) / (weighted_diff * (start-end));
+    if(score < 0)
+        return 0;
+    return score;
 }
-*/
+
