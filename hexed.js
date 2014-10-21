7ea3c1066b5f370dd180212c278348961e0dc6a2
wonapa <anantaya22@gmail.com>
2014-10-20 21:48:30 -0400

    some clean up

--- hexed.js
+++ hexed.js
@@ -1,3 +1,19 @@
+/*
+To-Do:
+
+[Done] Text boxes do not change the input when changed, and they can go outside 0-255.
+ 
+[Done] Text boxes display as decimal (the spec states that they need to be hex)
+
+There seems to be a bug in the scoring system on my branch (unimplemented in master).
+
+Many of the elements are still in the underlying DOM, and they need to be created as part of whatever object calls hexed().
+
+Difficulty and turns remain unimplemented.
+*/
+
+
+
 (function($){
 
     $.fn.hexed = function(arg) {
@@ -14,33 +30,43 @@
                 $.error('Action '+ action +' does not exist on jQuery.hexed');
             }
         }
+        //alert("difficulty: " + difficulty + " | turns: " + turns);
         return this;
     }
 
     function init() {
         make_sliders();
+        var turn_count = 0;
         $("#gen").click(function() { // need time
-            start = new Date().getMilliseconds();
-            var c=document.getElementById("goalCanvas");
-            var ctx=c.getContext("2d");
-            ctx.beginPath();
-            ctx.arc(100,75,50,0,2*Math.PI);
-        /* //This isn't working for some reason
-        ctx.strokeStyle="#fff";
-        ctx.lineWidth=1.5px;
-            ctx.stroke();
-        */
-        ctx.fillStyle=randomColor();
-            ctx.fill();
+            turns = $("#turns").val();
+            //alert("turns: " + turns);
+            difficulty = $("#difficulty").val();
+            turn_count++;
+            if (turn_count <= turns) {
+                start = new Date().getMilliseconds();
+                var c=document.getElementById("goalCanvas");
+                var ctx=c.getContext("2d");
+                ctx.beginPath();
+                ctx.arc(100,75,50,0,2*Math.PI);
+                /* //This isn't working for some reason
+                ctx.strokeStyle="#fff";
+                ctx.lineWidth=1.5px;
+                ctx.stroke();
+                */
+                ctx.fillStyle=randomColor();
+                ctx.fill();
+            }
         });
         $("#answer").click(function() {
             end = new Date().getMilliseconds();
         $("#score").text("Score: " + calculate_score());
-        alert(calculate_score());
+        //alert(calculate_score());
         });
     }
 
-    theColor=[];                       
+    theColor=[];
+    ansColor=[];
+
     function randomColor(){ //determine random rgb
         var r=Math.floor((Math.random() * 255)); //range of 0-255
         var g=Math.floor((Math.random() * 255));
@@ -56,6 +82,9 @@
         var ctx=c.getContext("2d");
         ctx.beginPath();
         ctx.arc(250,75,30,0,2*Math.PI);
+        ansColor = [$("#red_slider").slider("value"), 
+                    $("#green_slider").slider("value"), 
+                    $("#blue_slider").slider("value")];
         ctx.fillStyle = "#" + $("#red_slider").slider("value").toString(16) +
     	$("#green_slider").slider("value").toString(16) +
     	$("#blue_slider").slider("value").toString(16);
@@ -63,27 +92,73 @@
     }
 
     function make_sliders() {
-        $("#red_slider").slider({ min: 0, max: 255, slide: function(event, ui) {
-    	$("#red_slider_number").val(ui.value);
-    	$("#slider").find(".ui-slider-handle").text(ui.value);
-    	playerColor();
-        }});
-        $("#green_slider").slider({ min: 0, max: 255, slide: function(event, ui) {
-    	$("#green_slider_number").val(ui.value);
-    	playerColor();
-        }});
-        $("#blue_slider").slider({ min: 0, max: 255, slide: function(event, ui) {
-    	$("#blue_slider_number").val(ui.value);
-    	playerColor();
-        }});
+        $("#red_slider").slider({ 
+            min: 0, 
+            max: 255, 
+            slide: function(event, ui) {
+        	   $("#red_slider_number").val(ui.value.toString(16));
+        	   //$("#red_slider").find(".ui-slider-handle").text(ui.value);
+        	   playerColor();
+            }
+        });
+        $("#green_slider").slider({ 
+            min: 0, 
+            max: 255, 
+            slide: function(event, ui) {
+            	$("#green_slider_number").val(ui.value.toString(16));
+                //$("#green_slider").find(".ui-slider-handle").text(ui.value);
+        	   playerColor();
+            }
+        });
+        $("#blue_slider").slider({ 
+            min: 0, 
+            max: 255, 
+            slide: function(event, ui) {
+            	$("#blue_slider_number").val(ui.value.toString(16));
+                //$("#blue_slider").find(".ui-slider-handle").text(ui.value);
+            	playerColor();
+            }
+        });
+
+        // bind input box with slider's value
+        $("#red_slider_number").change(function () {
+            if ($(this).val() > 255)
+                $(this).val(255);
+            else if ($(this).val() < 0)
+                $(this).val(0);
+            //var value = this.value.substring(1);
+            //console.log(value);
+            $("#red_slider").slider("value", parseInt($(this).val()));
+            playerColor();
+        });
+        $("#green_slider_number").change(function () {
+            if ($(this).val() > 255)
+                $(this).val(255);
+            else if ($(this).val() < 0)
+                $(this).val(0);
+            //var value = this.value.substring(1);
+            //console.log(value);
+            $("#green_slider").slider("value", parseInt($(this).val()));
+            playerColor();
+        });
+        $("#blue_slider_number").change(function () {
+            if ($(this).val() > 255)
+                $(this).val(255);
+            else if ($(this).val() < 0)
+                $(this).val(0);
+            //var value = this.value.substring(1);
+            //console.log(value);
+            $("#blue_slider").slider("value", parseInt($(this).val()));
+            playerColor();
+        });
     }
 
     //determine the percent difference between the actual and
     //expected r, g, b variables
     function percent_off() {
-        return ((theColor[0] - $("#red_slider").slider("value"))/255 +
-                (theColor[1] - $("#green_slider").slider("value"))/255 +
-                (theColor[2] - $("#blue_slider").slider("value"))/255) * 100 / 3;
+        return (Math.abs(theColor[0] - ansColor[0])/255 +
+                Math.abs(theColor[1] - ansColor[1])/255 +
+                Math.abs(theColor[2] - ansColor[2])/255) * 100 / 3;
         /* division by 765 because it would otherwise be divided by 255 and 
            then by 3 to take the average */
     }
@@ -93,6 +168,6 @@
     function calculate_score() {
         var weighted_diff = 15 - difficulty;
         var score = (weighted_diff - percent_off()) / (weighted_diff * (end-start));
-        return percent_off() + "% off";
+        return percent_off() + "% off | " + score + " points";
     }
 }( jQuery ));
