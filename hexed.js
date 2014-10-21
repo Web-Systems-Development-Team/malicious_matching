1055921759b1503c9cdb4616c8ea583eb9408d37
aosdict <aosdict@gmail.com>
2014-10-20 20:37:23 -0400

    with '#' will be ignored, and an empty message aborts the commit.

--- hexed.js
+++ hexed.js
@@ -5,7 +5,7 @@
             var settings = arg;
             difficulty = settings.difficulty;
             turns = settings.turns;
-            init();
+            init(this);
         } else {
             var action = arg;
             if(action == "name of a command") {
@@ -17,31 +17,50 @@
         return this;
     }
 
-    function init() {
+    function init(hexObj) {
+	//Define r,g,b variables to hold the player entered RGB.
+	//When a player manipulates the text box or slider, this changes it.
+	//When a function needs to read from the RGB values, it reads from this.
+	red = green = blue = 255;
+
+	//Create the slider objects.
         make_sliders();
+
+	//Initialize the text boxes.
+	$("#red_slider_number").val(red);
+	$("#green_slider_number").val(green);
+	$("#blue_slider_number").val(blue);
+
+	//Add the drawing canvas and other elements.
+	hexObj.append("<canvas id=\"goalCanvas\" width=\"300\" height=\"150\"></canvas>");
+	hexObj.append("<div id=\"red_slider\" class=\"slider\"></div><input type=\"number\" id=\"red_slider_number\" value=\"0\">");
+	hexObj.append("<div id=\"green_slider\" class=\"slider\"></div><input type=\"number\" id=\"green_slider_number\" value=\"0\">");
+	hexObj.append("<div id=\"blue_slider\" class=\"slider\"></div><input type=\"number\" id=\"blue_slider_number\" value=\"0\">");
+	hexObj.append("<div id=\"buttons\"><button type=\"button\" id=\"gen\">Generate Color!</button><button type=\"button\" id=\"answer\">Got it!</button></div><p id=\"score\" style=\"#f00\"></p>");
+	
+	//Generate Color button click
         $("#gen").click(function() { // need time
-            start = new Date().getTime();
+            start = new Date().getMilliseconds();
             var c=document.getElementById("goalCanvas");
             var ctx=c.getContext("2d");
             ctx.beginPath();
             ctx.arc(100,75,50,0,2*Math.PI);
-        /* //This isn't working for some reason
-        ctx.strokeStyle="#fff";
-        ctx.lineWidth=1.5px;
-            ctx.stroke();
-        */
-        ctx.fillStyle=randomColor();
+            ctx.fillStyle=randomColor();
             ctx.fill();
         });
+
+	//Got It button click
         $("#answer").click(function() {
-            end = new Date().getTime();
-        $("#score").text("Score: " + calculate_score());
-        alert(calculate_score());
+            end = new Date().getMilliseconds();
+            $("#score").text("Red: "+red_off().toFixed(1)+"% off, Green: "+green_off().toFixed(1)+"% off, Blue: "+blue_off().toFixed(1)+"% off, Score: "+calculate_score());
         });
     }
 
-    theColor=[];                       
-    function randomColor(){ //determine random rgb
+    //Holds the secret actual color values
+    theColor=[];
+
+    //determine random rgb                     
+    function randomColor(){
         var r=Math.floor((Math.random() * 255)); //range of 0-255
         var g=Math.floor((Math.random() * 255));
         var b=Math.floor((Math.random() * 255));
@@ -49,50 +68,50 @@
         return "#" + r.toString(16) + g.toString(16) + b.toString(16);
     }
 
-    //Draws the player's color choice from the sliders next to the original color
+    //Draws the player's color choice from the sliders next to the original
+    //color
     function playerColor() {
-        //alert("playerColoe");
         var c=document.getElementById("goalCanvas");
         var ctx=c.getContext("2d");
         ctx.beginPath();
         ctx.arc(250,75,30,0,2*Math.PI);
-        ctx.fillStyle = "#" + $("#red_slider").slider("value").toString(16) +
-    	$("#green_slider").slider("value").toString(16) +
-    	$("#blue_slider").slider("value").toString(16);
+        ctx.fillStyle =
+	    "#" + red.toString(16) + green.toString(16) + blue.toString(16);
         ctx.fill();
     }
 
     function make_sliders() {
-        $("#red_slider").slider({ min: 0, max: 255, slide: function(event, ui) {
-    	$("#red_slider_number").val(ui.value);
-    	$("#slider").find(".ui-slider-handle").text(ui.value);
-    	playerColor();
+        $("#red_slider").slider({ min: 0, max: 255, value: red, slide: function(event, ui) {
+	    red = ui.value;
+	    $("#red_slider_number").val(red);
+    	    playerColor();
         }});
-        $("#green_slider").slider({ min: 0, max: 255, slide: function(event, ui) {
-    	$("#green_slider_number").val(ui.value);
-    	playerColor();
+        $("#green_slider").slider({ min: 0, max: 255, value: green, slide: function(event, ui) {
+	    green = ui.value;
+    	    $("#green_slider_number").val(green);
+    	    playerColor();
         }});
-        $("#blue_slider").slider({ min: 0, max: 255, slide: function(event, ui) {
-    	$("#blue_slider_number").val(ui.value);
-    	playerColor();
+        $("#blue_slider").slider({ min: 0, max: 255, value: blue, slide: function(event, ui) {
+	    blue = ui.value;
+    	    $("#blue_slider_number").val(blue);
+    	    playerColor();
         }});
     }
 
     //determine the percent difference between the actual and
     //expected r, g, b variables
+    function red_off() { return (red - theColor[0]) * 100 / 255; }
+    function green_off() { return (green - theColor[1]) * 100 / 255; }
+    function blue_off() { return (blue - theColor[2]) * 100 / 255; }
     function percent_off() {
-        return ((theColor[0] - $("#red_slider").slider("value"))/255 +
-                (theColor[1] - $("#green_slider").slider("value"))/255 +
-                (theColor[2] - $("#blue_slider").slider("value"))/255) * 100 / 3;
-        /* division by 765 because it would otherwise be divided by 255 and 
-           then by 3 to take the average */
+        return (Math.abs(red_off()) + Math.abs(green_off()) + Math.abs(blue_off())) / 3;
     }
 
     // determine score
     // ((15 – difficulty – percent_off) / (15 – difficulty)) * (15000 – milliseconds_taken)
     function calculate_score() {
         var weighted_diff = 15 - difficulty;
-        var score = (weighted_diff - percent_off()) / (weighted_diff * (end-start));
-        return percent_off() + "% off";
+        var score = ((weighted_diff - percent_off()) / weighted_diff) * (15000 - (end-start));
+        return score;
     }
 }( jQuery ));
